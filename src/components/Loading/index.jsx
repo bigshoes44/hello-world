7bb785a1fcecdc215fba102f09b7c4750f251c4f
bigshoes44 <larissa.info28@gmail.comgit config -- global user.name bigshoes44git config --global user.email larissa.info28@gmail.com>
2024-02-16 11:51:38 -0300

    Projeto imcompleto mas tentei dá o meu melhor

--- src/components/Loading/index.jsx
+++ src/components/Loading/index.jsx
@@ -1,12 +1,12 @@
 import "./styles.css";
-import { loading } from '/public';
+
 
 
 const Loading = () => {
     return (
 
         <div className="loader_container">
-            <img className="loader" src={loading} alt="Carregando" />
+            <img className="loader" src=loading alt="Carregando" />
         </div>
     );
 
