ab21180634face5d8c0b25c1619d53a992bb6ab4
bigshoes44 <larissa.info28@gmail.comgit config -- global user.name bigshoes44git config --global user.email larissa.info28@gmail.com>
2024-02-22 16:02:44 -0300

    Tentando resolver o B.O do loading

--- src/pages/PokemonPage/index.jsx
+++ src/pages/PokemonPage/index.jsx
@@ -43,7 +43,7 @@ const PokemonPage = () => {
             }
 
             setListaPokemons(listaTemporaria);
-            setRemoveLoading(true);
+            
         };
 
         useEffect(() => {
